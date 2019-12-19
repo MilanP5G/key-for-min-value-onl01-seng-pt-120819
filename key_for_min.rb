ace5cb09af486bd44afe743ec5fa05a4827c5f77
Milan Parmar <milan_parmar@hotmail.co.uk>
2019-12-19 13:55:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   
-  lowest_value = 0 
+  low_value = 0 
   lowest_key = nil 
   name_hash.collect do |key, value|
     if lowest_value == 0 || value < lowest_value 
